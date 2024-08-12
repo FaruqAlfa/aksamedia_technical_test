31bc69511e2d84556e7f5951a6e1e988e962cbaa
FaruqAlfa <alfafahmi172@gmail.com>
2024-08-12 20:49:17 +0700

    update division controller

--- app/Http/Controllers/DevisionsController.php
+++ app/Http/Controllers/DevisionsController.php
@@ -2,14 +2,28 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Divisions;
 use Illuminate\Http\Request;
 
 class DevisionsController extends Controller
 {
-    public function index()
+    public function index(Request $request)
     {
-        return response()->json([
-            'status' => 'success',
-        ]);
+       //ambil parameter nama untuk filter
+       $name = $request->input('name');
+
+       //ambil data devisi berdasarkan parameter name
+       $divisions = Divisions::when($name, function ($query, $name){
+           $query->where('name', 'like', '%'.$name.'%');
+       })->paginate(10);
+
+       return response()->json([
+           'status' => 'success',
+           'message' => 'Data divisions retrieved successfully',
+           'data' => [
+               'divisions' => $divisions->items()
+           ],
+           'pagination' => $divisions->toArray(),
+       ]);
     }
 }
