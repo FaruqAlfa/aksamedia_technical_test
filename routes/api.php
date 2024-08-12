6ee98b72fb602155978b57ce8ec029807b09ab15
FaruqAlfa <alfafahmi172@gmail.com>
2024-08-12 20:49:34 +0700

    update api

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,12 @@
 <?php
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
-
+use App\Http\Controllers\DevisionsController;
 
 Route::post('/login', [AuthController::class, 'login']);
 
+Route::middleware('auth:sanctum')->group(function () {
+    Route::get('/divisions', [DevisionsController::class, 'index']);
+
+});
 ?>
